25dbdf4803e8ca7ccc46536f75401113ef94ab49
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-09 19:28:10 -0500

    Made new app. Wrote names of models.

---
+++ plastid_ir_search/search_function/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class SearchResult(models.Model):
+    accession_number = models.CharField(max_length=200)
+
+
+class SearchHistory(models.Model):
+    search_result = models.ForeignKey(SearchResult, on_delete=models.CASCADE)
+
+class GenBankDatabase(models.Model):
+    accession_number = models.CharField(max_length=200)
