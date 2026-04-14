eb0c3beb2670a2b80c70700a2366a6b146064d0d
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-14 11:54:57 -0500

    Added image to front page.

--- plastid_ir_search/search_function/models.py
+++ plastid_ir_search/search_function/models.py
@@ -4,5 +4,6 @@ class SearchResult(models.Model):
     accession_number = models.CharField(max_length=200)
 
 
+
 class SearchHistory(models.Model):
     search_result = models.ForeignKey(SearchResult, on_delete=models.CASCADE)
