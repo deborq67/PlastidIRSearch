1f33f42bf0955b9b61ab858c8130dcc9c8d7c2a8
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-21 20:45:50 -0500

    Successfully added download features for results and history. Need to work on one for IR files too.

--- plastid_ir_search/plastid_ir_search/urls.py
+++ plastid_ir_search/plastid_ir_search/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from search_function.views import search, index, history
+from search_function.views import search, index, history, download_history, download_results
 from genbank_interaction.views import ir_info
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('results/', search, name='search'),
+    path('results/download/', download_results, name='download_results'),
     path('results/<str:accession>/', ir_info, name='info'),
     path('history/', history, name='history'),
+    path('history/download/', download_history, name='download_history'),
 ]
--- plastid_ir_search/search_function/views.py
+++ plastid_ir_search/search_function/views.py
@@ -3,12 +3,21 @@ from .plastid_search_function import initiate_search
 from .models import SearchResult, SearchHistory
 from genbank_interaction.models import IR_Identification
 from datetime import datetime
+import csv
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
 
 def search(request):
     if request.method == 'POST':
+
+        if SearchResult.objects.exists():
+            ''' Clear the SearchResult model at the beginning of each new search to
+             keep it from being too bloated. It's only meant to
+            link to SearchHistory anyway, which is persistent.'''
+            SearchResult.objects.all().delete()
+
         # Take out white space if user makes a search. Convert to dictionary for model conversion.
         search_term = request.POST.get('search_term', '').strip()
         search_query, total_records = initiate_search(search_term)
@@ -43,11 +52,6 @@ def search(request):
         #Save history.
         history_record.results.set(result_instances)
 
-        #Clear the SearchResult model to keep it from being too bloated. It's only meant to
-        #link to SearchHistory anyway, which is persistent.
-
-        # SearchResult.objects.all().delete()
-
         return render(request, 'search_function/results.html', {
             'search_term': search_term,
             'results': search_dict,
@@ -61,3 +65,39 @@ def history(request):
         session_key=request.session.session_key
     ).values('search_term', 'total_records', 'searched_at').order_by('-searched_at')
     return render(request, 'search_function/history.html', {"history_records": history_records})
+
+def download_results(request):
+    if request.GET.get('download'):
+        model_class = SearchResult
+
+        meta = model_class._meta
+        field_names = ['accession','title','bp_length','updated','created']
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="plastid_ir_search_results.csv"'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in model_class.objects.all():
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+    return render(request, "search_function/download.html")
+
+
+def download_history(request):
+    if request.GET.get('download'):
+        model_class = SearchHistory
+        meta = model_class._meta
+        field_names = ['search_term','total_records','searched_at']
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="plastid_ir_search_history.csv"'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in model_class.objects.all():
+            writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+    return render(request, "search_function/download.html")
