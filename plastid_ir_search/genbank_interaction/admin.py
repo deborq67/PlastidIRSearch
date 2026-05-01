2e2fe85cf579d827c78c05969be90ec5505c9f82
deborq67 <debohorquez@mail.fhsu.edu>
2026-04-30 22:42:25 -0500

    The admin is complete.

--- plastid_ir_search/genbank_interaction/admin.py
+++ plastid_ir_search/genbank_interaction/admin.py
@@ -1,17 +1,42 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 from .models import IR_Identification
+from django.forms import DateInput
+from django import forms
+
+class IRIdentificationForm(forms.ModelForm):
+    class Meta:
+        model = IR_Identification
+        fields = "__all__"
+        widgets = {
+            "updated": DateInput(attrs={"type": "date"}),
+        }
+
 
 @admin.register(IR_Identification)
 class IRCalculationAdmin(ModelAdmin):
-    exclude = ["ir_reported", "irb_reported"]
+    form = IRIdentificationForm
+
+    list_display = ["accession", "title", "updated_date", "ir_reported"]
+    list_filter = ["updated", "ir_reported"]
+    list_editable = ["ir_reported"]
+    exclude = ["ira_reported" ,"irb_reported"]
+
     conditional_fields = {
-        "ira_reported_start": "ira_reported == 'yes' || ira_reported == 'exception' ",
-        "ira_reported_end": "ira_reported == 'yes' || ira_reported == 'exception'",
-        "ira_reported_length": "ira_reported == 'yes' || ira_reported == 'exception'",
-        "irb_reported_start": "ira_reported == 'yes'",
-        "irb_reported_end": "ira_reported == 'yes'",
-        "irb_reported_length": "ira_reported == 'yes'",
+        "ira_reported": "ir_reported == 'yes' || ir_reported == 'exception'",
+        "ira_reported_start": "ir_reported == 'yes' || ir_reported == 'exception' ",
+        "ira_reported_end": "ir_reported == 'yes' || ir_reported == 'exception'",
+        "ira_reported_length": "ir_reported == 'yes' || ir_reported == 'exception'",
+        "irb_reported_start": "ir_reported == 'yes'",
+        "irb_reported_end": "ir_reported == 'yes'",
+        "irb_reported_length": "ir_reported == 'yes'",
     }
     search_fields = ["accession", "title"]
+
+    #Make date ONLY appear.
+
+    def updated_date(self, obj):
+        return obj.updated.strftime('%B %d, %Y') if obj.updated else None
+    updated_date.short_description = "Last Updated"
+
     pass
--- plastid_ir_search/search_function/admin.py
+++ plastid_ir_search/search_function/admin.py
@@ -5,6 +5,6 @@ from .models import SearchResult, SearchHistory
 
 @admin.register(SearchHistory)
 class SearchHistoryAdmin(ModelAdmin):
-    readonly_fields = ["session_key", "search_term", "total_records"]
+    readonly_fields = ["session_key", "search_term", "total_records", "searched_at"]
     search_fields = ["search_term", "searched_at"]
     pass
